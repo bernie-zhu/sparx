315a21ff896d3b06337ad040709d2256dd9cc218
zaidbarkat174 <zaidbarkat174@gmail.com>
2021-06-09 11:33:32 -0500

    Add way to get popular target from patents

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-from requests_html import AsyncHTMLSession
-
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import scrapeCompanyLinks
+from CompanyTargetsDictionary import PatentTargets
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(scrapeCompanyLinks.CompanyLinksToClaims())
+    print(PatentTargets.linksintodict())
